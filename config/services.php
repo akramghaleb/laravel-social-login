56502a5f403c6170e9b7bb87550b880d91e20d5a
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2022-08-29 22:22:24 +0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
         'redirect' => 'GITHUB_REDIRECT_URL',
     ],
+    'google'=>[
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => 'GOOGLE_REDIRECT_URL',
+    ]
 ];
