76ee0cad1be3affd0263dac786c226b31c09d899
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2022-08-29 22:21:20 +0300

    Update services.php

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://example.com/callback-url',
+        'redirect' => 'GITHUB_REDIRECT_URL',
     ],
 ];
